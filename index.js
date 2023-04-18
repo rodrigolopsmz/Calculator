1aa3f63ace5b9aae1fb662fc2e364ce34c9f60d5
rodrigolopsmz <rodrigo.lop.sm@gmail.com>
2023-04-19 01:31:10 +0200

    created webpack bundling for react

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+function App() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+  const root = ReactDOM.createRoot(document.getElementById('app'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
