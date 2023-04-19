04fe83eb49d33bf0d68371cf5efdf0df3bc9f678
rodrigolopsmz <rodrigo.lop.sm@gmail.com>
2023-04-19 16:09:33 +0200

    removed eval function to process calculus. Avoid these because it is dangeroues. Instead programmed a recursive algorithm to solve this issue

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -11,10 +11,55 @@ const currentNumberTyped = (stringOperation) => {
     });
     return currentNumber;
 }
+const parseArray = (array)=>{
+    if(array.length === 1) return Number(array[0])
+    const indexPlus = array.indexOf("+");
+    const indexMinus = array.indexOf("-");
+    if (indexPlus !== -1)
+    {
+        const FirstTerm = parseArray(array.slice(0,indexPlus))
+        const SecondTerm = parseArray(array.slice(indexPlus+1))
+        return FirstTerm + SecondTerm
+    }
+    else if (indexMinus !== -1)
+    {
+        const FirstTerm = parseArray(array.slice(0,indexMinus))
+        const SecondTerm = parseArray(array.slice(indexMinus+1))
+        return FirstTerm - SecondTerm
+    }
+    else if (array[1] === '*') return (parseArray([array[0]]) * parseArray(array.slice(2)))
+    else if (array[1] === '/') return (parseArray([array[0]]) / parseArray(array.slice(2)))
+   
+
+}
+const convertStringOperationToValue = (stringValue)=>{
+    let currentNumber = ''
+    const arrayOfOperations = []
+    const stringCopy = [...stringValue]
+    stringCopy.forEach((character,index) => {
+        if (/^\d+$/.test(character) || character==='.') 
+        {
+            currentNumber = currentNumber + character
+            if (index === (stringCopy.length - 1))
+            {
+                arrayOfOperations.push(currentNumber)
+            }
+        }
+        else {
+            arrayOfOperations.push(currentNumber)
+            arrayOfOperations.push(character)
+            currentNumber = ''
+        }
+    });
+    
+    
+    return parseArray(arrayOfOperations);
+
+}
 const obtainValue = (stringOperation) => {
     if (/^\d+$/.test(stringOperation.slice(-1)) ){ 
 
-        var value = eval(stringOperation)
+        var value = convertStringOperationToValue(stringOperation)
         if (value !== Infinity && !isNaN(value)) return value
         else return 'Math Error'
     }
